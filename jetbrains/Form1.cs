d19125da01baddd0c7877545ff0fa1edf313bb53
Raghav Kundra <raghavkundrasan@gmail.com>
2019-07-21 13:27:42 +0530

    simpilified variable names v1

--- jetbrains/Form1.cs
+++ jetbrains/Form1.cs
@@ -31,7 +31,7 @@ namespace jetbrains
         private int H;
         public Form1()
         {
-            listener.Prefixes.Add("http:localhost:8080/");
+            listener.Prefixes.Add("http://localhost:8080/");
             listener.AuthenticationSchemes = AuthenticationSchemes.Basic;
             listener.Start();
             while(true)
@@ -97,11 +97,11 @@ namespace jetbrains
         }
         private RectangleF Crect(int x, int y, int width, int height)
         {
-            return new RectangleF((float)x, (float)y, (float)width, (float)height);
+            return new RectangleF(x, y, width,height);
         }
         public void pd_PrintPage(object sender, PrintPageEventArgs ev)
         {
-            Dictionary<string, List<string>> dictionary = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(this.s);
+            Dictionary<string, List<string>> dictionary = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(s);
             string str1 = dictionary["POS"][0];
             string str2 = dictionary["TableNo"][0];
             string str3 = dictionary["OrderNo"][0];
@@ -116,103 +116,103 @@ namespace jetbrains
             Font font5 = new Font("Arial", 6f, GraphicsUnit.Point);
             SolidBrush solidBrush = new SolidBrush(Color.Black);
             Pen pen = new Pen(Color.Black);
-            ev.Graphics.DrawRectangle(pen, this.X, this.Y, 228, 1169);
+            ev.Graphics.DrawRectangle(pen, X, Y, 228, 1169);
             StringFormat format1 = new StringFormat();
             format1.Alignment = StringAlignment.Center;
             StringFormat format2 = new StringFormat();
             format2.Alignment = StringAlignment.Near;
             StringFormat format3 = new StringFormat();
             format3.Alignment = StringAlignment.Far;
-            this.rectf = this.Crect(this.X, this.Y, this.W, 20);
-            ev.Graphics.DrawString("TOURIST VILLAGE SHIVPURI:", font1, Brushes.Blue, this.rectf, format1);
-            this.rectf = this.Crect(this.X, this.Y += 15, this.W, 20);
-            ev.Graphics.DrawString("Near Bhadalaya Kund, Chatri Road,", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X, this.Y += 10, this.W, 20);
-            ev.Graphics.DrawString("Shivpuri, India, 473551", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X, this.Y += 10, this.W, 20);
-            ev.Graphics.DrawString("+919999999999", font2, Brushes.Black, this.rectf, format1);
-            int x1 = this.X;
-            this.Y += 5;
-            for (; x1 < this.W; x1 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x1, (float)this.Y);
-            int width = this.W - 20;
-            this.rectf = this.Crect(this.X, this.Y += 10, width, 20);
-            ev.Graphics.DrawString("Place of supply: " + str1.ToString(), font2, Brushes.Black, this.rectf, format2);
-            ev.Graphics.DrawString("Table No: " + str2.ToString(), font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X, this.Y += 10, width, 20);
-            ev.Graphics.DrawString("Order No: " + str3.ToString(), font2, Brushes.Black, this.rectf, format2);
-            ev.Graphics.DrawString("Date:  " + str4.ToString(), font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X, this.Y += 10, width, 20);
-            ev.Graphics.DrawString("Delivery Date: " + str5.ToString(), font2, Brushes.Black, this.rectf, format2);
-            ev.Graphics.DrawString("Time: " + str6.ToString(), font2, Brushes.Black, this.rectf, format3);
-            int x2 = this.X;
-            this.Y += 10;
-            for (; x2 < this.W; x2 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x2, (float)this.Y);
-            this.rectf = this.Crect(this.X, this.Y += 10, width / 2 + 30, 20);
-            ev.Graphics.DrawString("Item Name", font2, Brushes.Black, this.rectf, format1);
-            ev.Graphics.DrawString("QTY", font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString("Price", font2, Brushes.Black, this.rectf, format2);
-            ev.Graphics.DrawString("Amt", font2, Brushes.Black, this.rectf, format3);
-            int x3 = this.X;
-            this.Y += 10;
-            for (; x3 < this.W; x3 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x3, (float)this.Y);
+            rectf = Crect(X, Y, W, 20);
+            ev.Graphics.DrawString("TOURIST VILLAGE SHIVPURI:", font1, Brushes.Blue, rectf, format1);
+            rectf = Crect(X, Y += 15, W, 20);
+            ev.Graphics.DrawString("Near Bhadalaya Kund, Chatri Road,", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X, Y += 10, W, 20);
+            ev.Graphics.DrawString("Shivpuri, India, 473551", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X, Y += 10, W, 20);
+            ev.Graphics.DrawString("+919999999999", font2, Brushes.Black, rectf, format1);
+            int x1 = X;
+            Y += 5;
+            for (; x1 < W; x1 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, x1, Y);
+            int width = W - 20;
+            rectf = Crect(X, Y += 10, width, 20);
+            ev.Graphics.DrawString("Place of supply: " + str1.ToString(), font2, Brushes.Black, rectf, format2);
+            ev.Graphics.DrawString("Table No: " + str2.ToString(), font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X, Y += 10, width, 20);
+            ev.Graphics.DrawString("Order No: " + str3.ToString(), font2, Brushes.Black, rectf, format2);
+            ev.Graphics.DrawString("Date:  " + str4.ToString(), font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X, Y += 10, width, 20);
+            ev.Graphics.DrawString("Delivery Date: " + str5.ToString(), font2, Brushes.Black, rectf, format2);
+            ev.Graphics.DrawString("Time: " + str6.ToString(), font2, Brushes.Black, rectf, format3);
+            int x2 = X;
+            Y += 10;
+            for (; x2 < W; x2 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, x2, Y);
+            rectf = Crect(X, Y += 10, width / 2 + 30, 20);
+            ev.Graphics.DrawString("Item Name", font2, Brushes.Black, rectf, format1);
+            ev.Graphics.DrawString("QTY", font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString("Price", font2, Brushes.Black, rectf, format2);
+            ev.Graphics.DrawString("Amt", font2, Brushes.Black, rectf, format3);
+            int x3 = X;
+            Y += 10;
+            for (; x3 < W; x3 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x3, (float)Y);
             List<string> stringList = new List<string>();
             for (int index = 0; index < dictionary["itemnames"].Count - 1; index += 4)
                 stringList.Add(dictionary["itemnames"][index]);
-            this.Y += 10;
+            Y += 10;
             for (int index = 0; index < dictionary["itemnames"].Count; index += 4)
             {
-                this.rectf = this.Crect(this.X, this.Y, width / 2 + 30, 20);
-                ev.Graphics.DrawString(dictionary["itemnames"][index], font2, Brushes.Black, this.rectf, format1);
-                ev.Graphics.DrawString(dictionary["itemnames"][index + 1], font2, Brushes.Black, this.rectf, format3);
-                this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-                ev.Graphics.DrawString(dictionary["itemnames"][index + 2], font2, Brushes.Black, this.rectf, format2);
-                ev.Graphics.DrawString(dictionary["itemnames"][index + 3], font2, Brushes.Black, this.rectf, format3);
-                this.Y += 20;
+                rectf = Crect(X, Y, width / 2 + 30, 20);
+                ev.Graphics.DrawString(dictionary["itemnames"][index], font2, Brushes.Black, rectf, format1);
+                ev.Graphics.DrawString(dictionary["itemnames"][index + 1], font2, Brushes.Black, rectf, format3);
+                rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+                ev.Graphics.DrawString(dictionary["itemnames"][index + 2], font2, Brushes.Black, rectf, format2);
+                ev.Graphics.DrawString(dictionary["itemnames"][index + 3], font2, Brushes.Black, rectf, format3);
+                Y += 20;
             }
-            int x4 = this.X;
-            this.Y -= 10;
-            for (; x4 < this.W; x4 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x4, (float)this.Y);
-            this.Y += 10;
-            this.rectf = this.Crect(this.X, this.Y, width / 2, 20);
-            ev.Graphics.DrawString("Total: ", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString(dictionary["Total"][0], font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X, this.Y += 20, width / 2, 20);
-            ev.Graphics.DrawString("CGST @2.50% ", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString(dictionary["CGST @2.50%"][0], font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X, this.Y += 20, width / 2, 20);
-            ev.Graphics.DrawString("SGST @2.50%", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString(dictionary["SGST @2.50%"][0], font2, Brushes.Black, this.rectf, format3);
-            this.rectf = this.Crect(this.X, this.Y += 20, width / 2, 20);
-            ev.Graphics.DrawString("Total Tax", font2, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString(dictionary["Total Tax"][0], font2, Brushes.Black, this.rectf, format3);
-            int x5 = this.X;
-            this.Y += 10;
-            for (; x5 < this.W; x5 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x5, (float)this.Y);
-            this.rectf = this.Crect(this.X, this.Y += 10, width / 2 + 25, 20);
-            ev.Graphics.DrawString("NET PAYABLE(INR)", font1, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X + width / 2 + 45, this.Y, width / 2 - 45, 20);
-            ev.Graphics.DrawString(dictionary["Net Payable(INR)"][0], font1, Brushes.Black, this.rectf, format1);
-            int x6 = this.X;
-            this.Y += 10;
-            for (; x6 < this.W; x6 += 5)
-                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x6, (float)this.Y);
-            this.Y += 10;
-            this.rectf = this.Crect(this.X, this.Y, this.W, 20);
-            ev.Graphics.DrawString("THANK YOU!", font1, Brushes.Black, this.rectf, format1);
-            this.rectf = this.Crect(this.X, this.Y += 20, this.W, 20);
+            int x4 = X;
+            Y -= 10;
+            for (; x4 < W; x4 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x4, (float)Y);
+            Y += 10;
+            rectf = Crect(X, Y, width / 2, 20);
+            ev.Graphics.DrawString("Total: ", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString(dictionary["Total"][0], font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X, Y += 20, width / 2, 20);
+            ev.Graphics.DrawString("CGST @2.50% ", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString(dictionary["CGST @2.50%"][0], font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X, Y += 20, width / 2, 20);
+            ev.Graphics.DrawString("SGST @2.50%", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString(dictionary["SGST @2.50%"][0], font2, Brushes.Black, rectf, format3);
+            rectf = Crect(X, Y += 20, width / 2, 20);
+            ev.Graphics.DrawString("Total Tax", font2, Brushes.Black, rectf, format1);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString(dictionary["Total Tax"][0], font2, Brushes.Black, rectf, format3);
+            int x5 = X;
+            Y += 10;
+            for (; x5 < W; x5 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x5, (float)Y);
+            rectf = Crect(X, Y += 10, width / 2 + 25, 20);
+            ev.Graphics.DrawString("NET PAYABLE(INR)", font1, Brushes.Black, rectf, format1);
+            rectf = Crect(X + width / 2 + 45, Y, width / 2 - 45, 20);
+            ev.Graphics.DrawString(dictionary["Net Payable(INR)"][0], font1, Brushes.Black, rectf, format1);
+            int x6 = X;
+            Y += 10;
+            for (; x6 < W; x6 += 5)
+                ev.Graphics.DrawString("-", font5, Brushes.Black, (float)x6, (float)Y);
+            Y += 10;
+            rectf = Crect(X, Y, W, 20);
+            ev.Graphics.DrawString("THANK YOU!", font1, Brushes.Black, rectf, format1);
+            rectf = Crect(X, Y += 20, W, 20);
             string str7 = DateTime.Now.ToString("F");
             Console.WriteLine(str7);
-            ev.Graphics.DrawString("Printed on: " + str7, font2, Brushes.Black, this.rectf, format1);
+            ev.Graphics.DrawString("Printed on: " + str7, font2, Brushes.Black, rectf, format1);
             Console.WriteLine("success");
         }
 
